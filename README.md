c934aedd6892cc8b1d1e8603293e979685b6be3a
Saugat Rimal <86593756+saugat-rimal@users.noreply.github.com>
2021-07-18 02:50:39 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project: Todo List
+
+Simple todo list app, using local storage to persist data.
+
+## Links
+
+jQuery: http://jquery.com/
+
+localStorage: http://www.w3schools.com/html/html5_webstorage.asp
+
+## Challenge
+* Create a custom animation using `animate()`
+* Add a 4th question to the FAQ
+* Replace the text of one of the answers with an image or gif
---
+++ index.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+$("#list-items").html(localStorage.getItem("listItems"));
+
+$(".add-items").submit(function (event) {
+event.preventDefault();
+
+var item = $("#todo-list-item").val();
+
+if (item) {
+
+$("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+
+localStorage.setItem("listItems", $("#list-items").html());
+
+$("#todo-list-item").val("");
+
+}
+});
+
+
+//$(".checkbox").change(function(){
+//console.log("Checkbox checked!");
+//});
+
+
+$(document).on("change", ".checkbox", function(){
+  if ($(this).attr("checked")) {
+$(this).removeAttr("checked");
+
+  } else {
+$(this).attr("checked", "checked");
+
+}
+
+$(this).parent().toggleClass("completed");
+localStorage.setItem("listItems", $("#list-items").html());
+});
+
+
+
+$(document).on("click", ".remove", function(){
+$(this).parent().remove();
+localStorage.setItem("listItems", $("#list-items").html());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
